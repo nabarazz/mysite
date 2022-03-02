d72b614726ee8f3c5172de3a02f57847ddb6ca40
nabaraz01 <nabaraz01>
2022-03-02 15:42:39 +0545

    commit1

---
+++ demo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
